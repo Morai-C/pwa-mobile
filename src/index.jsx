51fbeb5bc45fdb6e45850abdaf8dc4c80ffdd1cc
itzemorai87@gmail.com <itzemorai87@gmail.com>
2022-11-22 22:50:52 +0100

    homepage

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
+import "./styles/style.scss";
 import App from "./App";
+import "@fortawesome/fontawesome-free/css/all.css";
+
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
